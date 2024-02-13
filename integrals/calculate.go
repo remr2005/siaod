4bf5e5d81edc77169cff4389cd7bfde0b23730ec
Kemran <morpeh2005ka@gmail.com>
2024-02-13 20:09:12 +0300

    Add AddaptSimpson

--- integrals/calculate.go
+++ integrals/calculate.go
@@ -43,7 +43,7 @@ func Simpson(inFunc func(float64) float64, a, b, n_min, n_max, e float64) (float
 		return res * h / 3
 	}
 	var Sh_last float64
-	for i := n_min; i < n_max; i++ {
+	for i := n_min; i < n_max; i *= 2 {
 		var Sh float64 = calculate(a, b, math.Abs(b-a)/i)
 		if math.Abs(Sh-Sh_last) < e {
 			return Sh, Runge(Sh, calculate(a, b, math.Abs(b-a)/(i*2)), "simpson"), nil
@@ -68,7 +68,29 @@ func MonteKarlo(inFunc func(x float64) float64, a, b, n float64, iter int) (floa
 	return arr[0], Sred(arr)
 }
 
-func SimpsonRec(inFunc func(float64) float64, a, b, n_min, n_max, e float64) float64 {
-
-	return 0.0
+func AdaptSimpson(inFunc func(float64) float64, a, b, k, e float64) (float64, float64) {
+	calculate := func(a, b, h float64) float64 {
+		res := inFunc(a)
+		var i int = 0
+		for a < b {
+			a += h
+			if i%2 == 0 {
+				res += inFunc(a) * 4
+			} else {
+				res += inFunc(a) * 2
+			}
+			i += 1
+		}
+		res += inFunc(b)
+		return res * h / 3
+	}
+	x := calculate(a, b, (a+b)/k)
+	y := calculate(a, b, (a+b)/(k*2))
+	if !(math.Abs(x-y) < 15*e) {
+		x, _ = AdaptSimpson(inFunc, a, (a+b)/2, k, e/2)
+		y, _ = AdaptSimpson(inFunc, (a+b)/2, b, k, e/2)
+		return x + y, 0.0
+	} else {
+		return y, Runge(x, y, "simson")
+	}
 }
--- main.go
+++ main.go
@@ -6,11 +6,15 @@ import (
 )
 
 func main() {
-	res, pog, err := integrals.Simpson(integrals.F1, 0, 1.6, 2, 1000, 0.0001)
+	res, pog, err := integrals.Simpson(integrals.F1, 0, 1.6, 2, 200000000, 0.000000000001)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(res, pog)
+
 	res, pog = integrals.MonteKarlo(integrals.F1, 0, 1.6, 100000, 10)
+	fmt.Println(res, pog)
+
+	res, pog = integrals.AdaptSimpson(integrals.F1, 0, 1.6, 200000, 0.000000000001)
 	fmt.Println(res, pog)
 }
