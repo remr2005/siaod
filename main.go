c0d61ba3410a6a244b874259db21c498de5b1999
Kemran <109353270+remr2005@users.noreply.github.com>
2024-03-14 20:50:09 +0300

    Сделал 4 лабу

--- main.go
+++ main.go
@@ -3,52 +3,16 @@ package main
 import (
 	"fmt"
 	"math"
-	"siaodMath/snau"
+	"siaodMath/interpolation"
 )
 
 func main() {
-	arr := make([]func([]float64) float64, 0)
-	arr = append(arr, f1)
-	arr = append(arr, f2)
-	// arr = append(arr, F1)
-	// arr = append(arr, F2)
-	// arr = append(arr, F3)
-	// arr = append(arr, F4)
-	// arr = append(arr, F5)
-	// arr = append(arr, F6)
-	fmt.Println("Ньютона -", snau.Newton(func(f float64) float64 { return 2*math.Log(f) - 1/f }, 2, 0.0001))
-	fmt.Println("Ньютона Равз -", snau.Rafs(arr, []float64{1, 1}, 0.0001, 10000))
-}
-
-// Вывод
-//0.19634944003810098 3.811902345625858e-09
-//0.19634940866640296 2.2137558286505765e-08
-//0.19642765260874218 1.3020805412109851e-05
-//0.19597239895846208 0.002043287065238123 (различается при запусках из за рандома)
-//0.19634891515714303 5.1453733246886735e-05
-
-func F1(x []float64) float64 {
-	return 2*x[0] + 1*x[1] + 3*x[2] + 4*x[3] + 5*x[4] + 6*x[5] - 20
-}
-func F2(x []float64) float64 {
-	return 0*x[0] + (-1)*x[1] + 2*x[2] + 3*x[3] + 1*x[4] + 2*x[5] + 3
-}
-func F3(x []float64) float64 {
-	return 1*x[0] + 0*x[1] + 1*x[2] + 2*x[3] + (-1)*x[4] + 1*x[5] - 12
-}
-func F4(x []float64) float64 {
-	return (-1)*x[0] + 2*x[1] + 0*x[2] + 1*x[3] + 3*x[4] + (-2)*x[5] - 5
-}
-func F5(x []float64) float64 {
-	return 4*x[0] + 2*x[1] + 1*x[2] + 0*x[3] + (-1)*x[4] + 3*x[5] - 8
-}
-func F6(x []float64) float64 {
-	return 3*x[0] + 1*x[1] + 2*x[2] + (-1)*x[3] + 0*x[4] + 4*x[5] - 10
-}
-
-func f1(x []float64) float64 {
-	return math.Sin(x[0]+1) - x[1] - 1.2
-}
-func f2(x []float64) float64 {
-	return 2*x[0] + math.Cos(x[1]) - 2
+	y := make([]float64, 0)
+	for _, i := range []float64{1.00, 1.05, 1.09, 1.13, 1.15, 1.17, 1.2} {
+		y = append(y, math.Exp(i))
+	}
+	f := interpolation.Larange([]float64{1.1, 1.4, 1.6, 1.7, 1.9}, []float64{1, 2, 3, 4, 5})
+	fmt.Println(f(1))
+	fmt.Println(interpolation.Splain([]float64{1.00, 1.05, 1.09, 1.13, 1.15, 1.17, 1.2}, y, []float64{1.05, 1.09, 1.13, 1.15, 1.17}))
+	fmt.Println(interpolation.Hermit(math.Exp, []float64{1.00, 1.05, 1.09, 1.13, 1.15, 1.17, 1.2}, []float64{1.05, 1.09, 1.13, 1.15, 1.17}))
 }
